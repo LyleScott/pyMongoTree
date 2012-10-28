cebc19843c5e4b6af6ed387042055a08c49d41b2
Lyle Scott III <lyle@digitalfoo.net>
2012-10-28 14:18:41 -0400

    Take out extra crap.

--- venv/venv.sh
+++ venv/venv.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 virtualenv --no-site-packages .
-pip install -r requirements
 source bin/activate
-PYTHONPATH=$PYTHONPATH:`pwd`/../src; export PYTHONPATH
-echo "PYTHONPATH: $PYTHONPATH"
+pip install -r requirements
