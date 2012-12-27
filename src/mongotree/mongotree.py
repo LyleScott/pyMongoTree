450e4d6b78e070be9a7961ee258eda3795b09e18
Lyle Scott III <lyle@digitalfoo.net>
2012-12-26 20:33:02 -0500

    general updates

--- src/mongotree/mongotree.py
+++ src/mongotree/mongotree.py
@@ -33,15 +33,15 @@ from pprint import pformat
 
 class MongoTree(object):
     """An implEementation of modeling MongoDB values as a Tree with nodes
-    that have 1 parent and N children.""" 
-    
+    that have 1 parent and N children."""
+
     SEPARATOR = '|$|'
-    
+
     def __init__(self, host='localhost', port=27017, db_name='mongotree',
                  uri=None, identifier='mongotree'):
         """Initialization routines.
-        
-        Args:
+
+        Arguments:
             host (string): Hostname that is serving the MongoDB instance.
             port (integer): The port to connect to host on.
             db_name (string): The database name on the MongoDB instance.
@@ -56,52 +56,52 @@ class MongoTree(object):
         else:
             self.mongo = pymongo.Connection(host=host, port=port)
             self.db = self.mongo[db_name]
-    
+
         self.identifier = identifier
-        
+
     def __repr__(self):
         s = []
         for root in self.get_roots():
             s.extend(self.traverse(root))
         return pformat(s)
-        
+
     def get_node(self, path):
         """Return a node at specific path.
-        
-        Args:
+
+        Arguments:
             path (sequence): A sequence of tokens representing a path to a node.
-        
+
         Returns:
             A dict representing a node || None.
         """
         path = self.SEPARATOR.join(path)
         key = {'identifier': self.identifier, 'path': path}
-        
+
         result = self.db.treefoo.find_one(key)
-        
+
         return result or None
-        
+
     def path_exists(self, path):
         """Check if a nodes exists at a specified path.
-        
-        Args:
+
+        Arguments:
             path (sequence): A sequence of tokens representing a path to a node.
-          
+
         Returns:
-            bool. 
+            bool.
         """
         path = self.SEPARATOR.join(path)
         key = {'identifier': self.identifier, 'path': path}
         return bool(self.db.treefoo.find_one(key))
-        
+
     def get_dotgraph(self, roots=None):
         """Generate a dot graph of the tree at a root via Graphviz.
-        
-        Args:
+
+        Arguments:
             roots (sequence): Roots to start graphing from.
                 Default: all roots.
         """
-        
+
         if roots and not getattr(roots, '__iter__', False):
             raise ValueError('get_dotgraph: roots argument must be a sequence')
 
@@ -110,59 +110,59 @@ class MongoTree(object):
             """
             for child in node['children']:
                 child = self.db.treefoo.find_one({'_id': child})
-                
+
                 parentNode = pydot.Node(str(node['_id']), label=node['label'])
                 graph.add_node(parentNode)
-                
+
                 childNode = pydot.Node(str(child['_id']), label=child['label'])
                 graph.add_node(childNode)
-                
+
                 edge = pydot.Edge(str(node['_id']), str(child['_id']))
                 graph.add_edge(edge)
-        
+
         graph = pydot.Dot(graph_type='digraph')
-        
+
         roots = roots or self.get_roots()
         for node in self.get_roots():
             self.traverse(node, function=add_children_nodes)
-            
+
         #graph.write_png('example1_graph.png')
-        
+
         return graph
-        
+
     def traverse(self, node, function=None, nodes=None):
         """Traverse the tree, optionally running a function on each node.
-        
-        Args:
+
+        Arguments:
             node (dict): A dict representing a parent node that will have its
                 children traversed (if there are any).
             function (function): A function to run on a node when it is
                 traversed.
-            nodes (list): Holds all nodes that have been recursively traversed.  
-        
+            nodes (list): Holds all nodes that have been recursively traversed.
+
         Returns:
             list. A list of all nodes that were traversed.
         """
         if nodes is None:
             nodes = []
         nodes.append(node)
-        
+
         if function:
             function(node)
 
-        # Traverse each child.        
+        # Traverse each child.
         for child in node['children']:
             child = self.db.treefoo.find_one({'_id': child})
             self.traverse(child, function=function, nodes=nodes)
-            
+
         return nodes
 
     def node_count(self, roots=None):
         """Return how many nodes there are in the tree.
-        
+
         Args (sequence): Optional. Instead of all roots, specify nodes to start
             the traversal from.
-        
+
         Returns:
             int
         """
@@ -170,34 +170,34 @@ class MongoTree(object):
         roots = roots or self.get_roots()
         for root in roots:
             n += len(self.traverse(root))
-            
+
         return n
 
     def get_roots(self):
         """Get the root nodes that contain the start of a tree.
-        
+
         Returns:
             list. A list of nodes with no parents (the "Top" nodes).
         """
         key = {'identifier': self.identifier, 'parent': None}
         return [row for row in self.db.treefoo.find(key)]
-        
+
     def upsert(self, path, obj=None, hit_inc=1):
         """Add a node to the tree.
-        
-        Args:
+
+        Arguments:
             path (sequence): A sequence of tokens representing a path to a node.
             obj (pickle): A pickled object stored as a blob on a node.
             hit_inc (int): Incremenet the nodes hit counter.
         """
         if not getattr(path, '__iter__', False):
             raise ValueError('upsert: path argument must be a sequence')
-            
+
         current_path = ''
         parent_objid = None
-        
+
         for token in path:
-        
+
             if current_path:
                 # Concatenate current_path with the incoming token.
                 current_path = self.SEPARATOR.join((current_path, token))
@@ -207,13 +207,13 @@ class MongoTree(object):
 
             # Try to find the row for this node.
             key = {'identifier': self.identifier, 'path': current_path}
-            
+
             # Values we want to store on the created/updated node.
             values = {}
-            
+
             # Increment the hit counter.
             values.setdefault('$inc', {})['hits'] = hit_inc
-            
+
             # Create a new row for the node if it doesn't exist.
             if not self.db.treefoo.find_one(key):
                 values.setdefault('$set', {})['label'] = token
@@ -225,87 +225,87 @@ class MongoTree(object):
                 values.setdefault('$set', {})['obj'] = obj
             else:
                 values.setdefault('$set', {})['obj'] = None
-            
+
             # Write to the db/collection.
             self.db.treefoo.update(key, values, upsert=True)
-        
+
             # Update the parent node to include this node as a child.
             if current_path:
                 obj_id = self.db.treefoo.find_one(key, {'_id': 1})['_id']
-                key = {'_id': parent_objid}    
+                key = {'_id': parent_objid}
                 values = {'$addToSet': {'children': obj_id}}
                 self.db.treefoo.update(key, values, upsert=True)
                 parent_objid = obj_id
-                
+
     def valid_node(self, node):
         """Indicate if a node is "valid", where valid indicates that it has all
         and only the keys it is supposed to have.
-        
-        Args:
+
+        Arguments:
             node (dict):  A dict representing a node.
-            
+
         Returns:
             bool
-        """ 
+        """
         node_keys = node.keys()
         valid_keys = ('label', 'path', 'parent', 'children', 'hits', 'obj',
                       'identifier', '_id')
-        
+
         if len(node_keys) != len(valid_keys):
             return False
-        
+
         for valid_key in valid_keys:
             if valid_key not in node_keys:
                 return False
-            
+
         return True
-        
+
     def remove(self, node):
         """Remove a node from the tree. If it has children, remove those, too.
-        
+
         Arg:
             node (dict):  A dict representing a node.
         """
         if not self.valid_node(node):
             raise ValueError('MongoTree::remove:> node used for the node '
                              'argument is not valid.')
-      
+
         for child in node['children']:
             child = self.db.treefoo.find_one({'_id': child})
             if not child:
                 continue
             self.remove(child)
-            
+
         self.db.treefoo.remove({'_id': node['_id']})
-    
+
     def parent(self, path):
         """Get the parent of a node.
-        
-        Args:
+
+        Arguments:
             path (sequence): A sequence of tokens representing a path to a node.
-        
+
         Returns:
             A dict representing a node || None.
         """
         path = self.SEPARATOR.join(path)
         key = {'identifier': self.identifier, 'path': path}
         result = self.db.treefoo.find_one(key)
-        
+
         if result:
             parent = result['parent']
             node = self.db.treefoo.find_one({'_id': parent})
             return node
-        
+
         return None
-                
+
     def children(self, node=None, path=None):
         """Get all children of a node.
-        
-        Args:
+
+        Arguments:
             node (dict):  A dict representing a node.
             path (string): The path to use to find a node with a specific
                 path.
-        
+
         Returns:
             list. All children of the node with path=parent_path.
         """
@@ -315,33 +315,34 @@ class MongoTree(object):
         else:
             if path and not hasattr(path, '__iter__'):
                 raise ValueError('children: path argument must be a sequence')
-                
+
         if node:
             return node['children']
-        
+
         node = self.get_node(path)
-               
+
         return node['children']
-        
+
     def fromXml(self, xml):
         """Build a tree from an XML string.
-        
-        Args:
+
+        Arguments:
             xml (string): A string of valid XML.
         """
         root = etree.fromstring(xml)
-        
+
         def add_nodes(root, path=None):
             """Get all parent/children node combinations and add to the
             tree collection.
             """
             if path is None:
                 path = []
-                
+
             nodes = path + [root,]
             self.upsert([node.tag for node in nodes])
 
             for child in root.getchildren():
                 add_nodes(child, path=nodes)
-                    
+
         add_nodes(root)
+
